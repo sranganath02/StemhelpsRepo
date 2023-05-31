13354448e3f1a743aa8d13a231f723bbea9c12b8
KVerduzco <135088298+KVerduzco@users.noreply.github.com>
2023-05-30 23:48:39 -0400

    Update main.py
    mE

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from PIL import ImageTk, Image
 window = Tk()
-window.title("kevin is better")
+window.title("kevin is better ")
 window.geometry("700x500")
 
 top = Toplevel()
